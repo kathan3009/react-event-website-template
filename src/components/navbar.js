3ed6a538f4387192245a9846d7c30803bc90e270
kathan3009 <kthndesai@gmail.com>
2020-12-26 16:58:07 +0530

    changes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,14 +6,13 @@ const Navbar = () => {
 
     return (
         <div className="header">
-            <h1 class="logo"><Link className="nav-link" to="/">TechFest</Link></h1>
-            <ul class="main-nav">
+            <h1 className="logo"><Link className="nav-link" to="/">TechFest</Link></h1>
+            <ul className="main-nav">
                 <li> <Link to='/events' className="nav-link">Events</Link> </li>
                 <li> <Link to='/member' className="nav-link">Members</Link> </li>
                 <li> <Link to='/faqs' className="nav-link">FAQS</Link> </li>
                 <li> <Link to='/contact' className="nav-link">Contact</Link> </li>
             </ul>
-
         </div>
     )
 
